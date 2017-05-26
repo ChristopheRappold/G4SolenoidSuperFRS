32bfe3449fe1b834daee564de441ee75076e41d3
C. Rappold <c.rappold@gsi.de>
2017-05-26 18:22:19 +0200

    Make runData.C ready for multiproccess for use in cluster
    Make ready the ROOT script runData.C for the 
    multiprocess use.

--- runData.C
+++ runData.C
@@ -16,10 +16,19 @@
 #include <string>
 #include <set>
 #include <unordered_map>
+#include <iostream>
+
+#include "ROOT/TSeq.hxx"
+//#include "ROOT/TThreadedObject.hxx"
+//#include "ROOT/TProcessExecutor.hxx"
+//#include "TTreeProcessorMT.hxx"
 
 void runData(const std::string& nameList, const std::set<std::string>& ParticleList = std::set<std::string>(), const std::string& nameOut = "" )
 {
+  //int nthreads = 4;
+  //ROOT::EnableImplicitMT(nthreads);
   
+
   TChain* Chain = new TChain("G4Tree");
   TFile* f_first = nullptr;
   std::cout << "Files :" << nameList << std::endl;
@@ -50,7 +59,7 @@ void runData(const std::string& nameList, const std::set<std::string>& ParticleL
 
   //TTree* tt = (TTree*) f->Get("G4Tree");
   std::vector<std::string>* nameDetInFile = (std::vector<std::string>*)(f_first->Get("nameDet"));
-  std:vector<std::string> nameDet ;
+  std::vector<std::string> nameDet ;
   for(auto name : *nameDetInFile)
     nameDet.emplace_back(name);
   f_first->Close();
@@ -58,14 +67,7 @@ void runData(const std::string& nameList, const std::set<std::string>& ParticleL
   std::cout<<" load nameDet done !\n";
   for(auto name : nameDet)
     std::cout<<name<<"\n";
-  
-  TTreeReader reader(Chain);
 
-  TTreeReaderValue<TG4Sol_Event> Revent(reader,"TG4Sol_Event");
-  std::vector<TTreeReaderArray<TG4Sol_Hit>*> AllHits;
-  for(auto name : nameDet)
-    AllHits.emplace_back(new TTreeReaderArray<TG4Sol_Hit>(reader,name.c_str()));
-  
   size_t id_Si3 = 0;
   size_t id_FMF2 = 0;
   size_t id_TrckFwd = 0; 
@@ -78,127 +80,200 @@ void runData(const std::string& nameList, const std::set<std::string>& ParticleL
       if(nameDet[id] == "HypHI_TrackFwd_log")
 	id_TrckFwd = id;
     }
-  
-  TH2F* h_HitPatternX = new TH2F("h_HitPatternX","h_HitPatternX",2000,-20,20,20,0,20);
-  TH2F* h_HitPatternY = new TH2F("h_HitPatternY","h_HitPatternY",2000,-20,20,20,0,20);
-  TH2F* h_ParticlePhi = new TH2F("h_Phi","h_Phi",360*5,-180,180,30,0,30);
-  
-  const auto Entries = Chain->GetEntries();
-  std::cout << " Entries :" << Entries << std::endl;
-  int timing = 0;
 
-  while(reader.Next())
-    {
-      int nb = reader.GetCurrentEntry();
-      auto event = Revent.Get();
-      if(static_cast<int>(static_cast<double>(nb) / static_cast<double>(Entries) * 10) == timing)
-        {
-          std::cout <<" Processing :" << timing * 10 << "%  Event #"<<nb<<" \n";
-          ++timing;
-        }
+    
+  //ROOT::TTreeProcessorMT tp(Chain);
+  TTreeReader reader(Chain);
+  
+  //ROOT::TThreadedObject<TH2F> h_HitPatternXAll("h_HitPatternX","h_HitPatternX",2000,-20,20,20,0,20);
+  //ROOT::TThreadedObject<TH2F> h_HitPatternYAll("h_HitPatternY","h_HitPatternY",2000,-20,20,20,0,20);
+  //ROOT::TThreadedObject<TH2F> h_ParticlePhiAll("h_Phi","h_Phi",360*5,-180,180,30,0,30);
+ 
+  auto f_Process = [&](TTreeReader& reader)
+		   {
+		     
+		     TTreeReaderValue<TG4Sol_Event> Revent(reader,"TG4Sol_Event");
+		     std::vector<TTreeReaderArray<TG4Sol_Hit>*> AllHits;
+		     for(auto name : nameDet)
+		       AllHits.emplace_back(new TTreeReaderArray<TG4Sol_Hit>(reader,name.c_str()));
+		     
+		     TH2F* h_HitPatternX = new TH2F("h_HitPatternX","h_HitPatternX",2000,-20,20,20,0,20);
+		     TH2F* h_HitPatternY = new TH2F("h_HitPatternY","h_HitPatternY",2000,-20,20,20,0,20);
+		     TH2F* h_ParticlePhi = new TH2F("h_Phi","h_Phi",360*5,-180,180,30,0,30);
+  
+		     // auto h_HitPatternX = h_HitPatternX.Get();
+		     // auto h_HitPatternY = h_HitPatternY.Get();
+		     // auto h_ParticlePhi = h_ParticlePhiAll.Get();
+		     
+		     const auto Entries = Chain->GetEntries();
+		     std::cout << " Entries :" << Entries << std::endl;
+		     int timing = 0;
+		     int first_event = 0;
+		     while(reader.Next())
+		       {
+			 int nb = reader.GetCurrentEntry();
+			 auto event = Revent.Get();
+			 if(static_cast<int>(static_cast<double>(nb) / static_cast<double>(Entries) * 10) == timing)
+			   {
+			     std::cout <<" Processing :" << timing * 10 << "%  Event #"<<nb<<" \n";
+			     ++timing;
+			   }
+			 
+			 std::set<int> validTrack;
+			 
+			 for(size_t id = 0;id<event->BeamNames.size();++id)
+			   {
+			     int trackID = event->BeamTrackID[id];
+			     //std::cout<<"beam : "<<event->BeamNames[id]<<" #"<<trackID<<"\n";
+			     if(ParticleList.size()>0)
+			       {
+				 auto it_par = ParticleList.find(event->BeamNames[id]);
+				 if(it_par != ParticleList.end())
+				   validTrack.insert(trackID);
+			       }
+			     else
+			       validTrack.insert(trackID);
+			     
+			   }
+			 for(size_t id = 0;id<event->DaughterNames.size();++id)
+			   {
+			     int trackID = event->DaughterTrackID[id];
+			     if(ParticleList.size()>0)
+			       {
+				 auto it_par = ParticleList.find(event->DaughterNames[id]);
+				 if(it_par != ParticleList.end())
+				   validTrack.insert(trackID);
+			       }
+			     else
+			       validTrack.insert(trackID);
+			   }
+			 
+			 for(size_t it_br = 0 ;it_br < AllHits.size(); ++it_br)
+			   {
+			     if(first_event==0)
+			       {
+				 TString nameBranch = AllHits[it_br]->GetBranchName();
+				 if(it_br == id_FMF2 || it_br == id_TrckFwd)
+				   {
+				     for(auto index : ROOT::TSeqI(3)) 
+				       {
+					 TString nameBranch2 = nameBranch;
+					 nameBranch2 += "_";
+					 nameBranch2 += index;
+					 std::cout<<"name "<<it_br<<" : "<<nameBranch2<<"\n";
+					 h_HitPatternX->Fill(h_HitPatternX->GetXaxis()->GetXmin()-10,nameBranch2,1.);
+					 h_HitPatternY->Fill(h_HitPatternY->GetXaxis()->GetXmin()-10,nameBranch2,1.);
+				       }
+				   }
+				 else
+				   {
+				     std::cout<<"name "<<it_br<<" : "<<nameBranch<<"\n";
+				     h_HitPatternX->Fill(h_HitPatternX->GetXaxis()->GetXmin()-10,nameBranch,1.);
+				     h_HitPatternY->Fill(h_HitPatternY->GetXaxis()->GetXmin()-10,nameBranch,1.);
+				   }
+			       }
+			     for(auto hit : *AllHits[it_br])
+			       {
+				 //hit.Print();
+				 auto it_find = validTrack.find(hit.TrackID);
+				 
+				 if(it_find != validTrack.end())
+				   {
+				     //std::cout<<"Branch: "<<Hits->GetBranchName()<<" "<<hit.HitPosX<<" "<<hit.HitPosY<<" "<<hit.HitPosZ<<" "<<" "<<hit.LayerID<<" "<<hit.Pname<<"\n";
+				     TString nameBranch = AllHits[it_br]->GetBranchName();
+				     if(it_br == id_FMF2 || it_br == id_TrckFwd)
+				       {
+					 nameBranch += "_";
+					 nameBranch += hit.LayerID;
+				       }
+				     h_HitPatternX->Fill(hit.HitPosX,nameBranch,1.);
+				     h_HitPatternY->Fill(hit.HitPosY,nameBranch,1.);
+				   }
+			       }
+			   }
+			 if(first_event==0)
+			   ++first_event;
 
-      std::set<int> validTrack;
+			 std::unordered_map<int,double> PhiPerTrack;
+			 
+			 for(auto hit : *AllHits[id_Si3])
+			   {
+			     auto it_find = validTrack.find(hit.TrackID);
+			     
+			     if(it_find != validTrack.end())
+			       {
+				 TVector3 TempMom(hit.MomX,hit.MomY,hit.MomZ);
+				 PhiPerTrack.insert(std::make_pair(hit.TrackID,TempMom.Phi()));
+			       }
+			   }
+			 for(auto hit : *AllHits[id_FMF2])
+			   {
+			     auto it_find = validTrack.find(hit.TrackID);
+			     
+			     if(it_find != validTrack.end())
+			       {
+				 if(hit.LayerID == 0)
+				   {
+				     TVector3 TempMom(hit.MomX,hit.MomY,hit.MomZ);
+				     auto it_phi = PhiPerTrack.find(hit.TrackID);
+				     if(it_phi != PhiPerTrack.end())
+				       {
+					 h_ParticlePhi->Fill((it_phi->second-TempMom.Phi())*TMath::RadToDeg(),hit.Pname.c_str(),1.);
+				       }
+				   }
+			       }
+			   }
+	  
+			 // for(size_t id = 0;id<event->DaughterNames.size();++id)
+			 // 	{
+			 // 	  int trackID = event->DaughterTrackID[id];
+			 // 	  std::cout<<"decayed : "<<event->DaughterNames[id]<<" #"<<trackID<<"\n";
+			 
+			 // 	  for(auto& Hits :AllHits)
+			 // 	    {
+			 // 	      for(auto hit : *Hits)
+			 // 		{
+			 // 		  //hit.Print();
+			 // 		  if(hit.TrackID==trackID)
+			 // 		    {
+			 // 		      std::cout<<"Branch: "<<Hits->GetBranchName()<<" "<<hit.HitPosX<<" "<<hit.HitPosY<<" "<<hit.HitPosZ<<" "<<hit.LayerID<<" "<<hit.Pname<<"\n";
+			 // 		    }
+			 // 		}
+			 // 	    }
+			 // 	}
+			 
+
       
-      for(size_t id = 0;id<event->BeamNames.size();++id)
-	{
-	  int trackID = event->BeamTrackID[id];
-	  //std::cout<<"beam : "<<event->BeamNames[id]<<" #"<<trackID<<"\n";
-	  if(ParticleList.size()>0)
-	    {
-	      auto it_par = ParticleList.find(event->BeamNames[id]);
-	      if(it_par != ParticleList.end())
-		validTrack.insert(trackID);
-	    }
-	  else
-	    validTrack.insert(trackID);
-	    
-	}
-       for(size_t id = 0;id<event->DaughterNames.size();++id)
-	 {
-	   int trackID = event->DaughterTrackID[id];
-	   if(ParticleList.size()>0)
-	    {
-	      auto it_par = ParticleList.find(event->DaughterNames[id]);
-	      if(it_par != ParticleList.end())
-		validTrack.insert(trackID);
-	    }
-	   else
-	     validTrack.insert(trackID);
-	 }
-       
-       for(size_t it_br = 0 ;it_br < AllHits.size(); ++it_br)
-	{
-	  for(auto hit : *AllHits[it_br])
-	    {
-	      //hit.Print();
-	      auto it_find = validTrack.find(hit.TrackID);
-	      
-	      if(it_find != validTrack.end())
-		{
-		  //std::cout<<"Branch: "<<Hits->GetBranchName()<<" "<<hit.HitPosX<<" "<<hit.HitPosY<<" "<<hit.HitPosZ<<" "<<" "<<hit.LayerID<<" "<<hit.Pname<<"\n";
-		  TString nameBranch = AllHits[it_br]->GetBranchName();
-		  if(it_br == id_FMF2 || it_br == id_TrckFwd)
-		    {
-		      nameBranch += "_";
-		      nameBranch += hit.LayerID;
-		    }
-		  h_HitPatternX->Fill(hit.HitPosX,nameBranch,1.);
-		  h_HitPatternY->Fill(hit.HitPosY,nameBranch,1.);
-		}
-	    }
-	}
-      std::unordered_map<int,double> PhiPerTrack;
+		       }
 
-      for(auto hit : *AllHits[id_Si3])
-	{
-	  auto it_find = validTrack.find(hit.TrackID);
-	  
-	  if(it_find != validTrack.end())
-	    {
-	      TVector3 TempMom(hit.MomX,hit.MomY,hit.MomZ);
-	      PhiPerTrack.insert(std::make_pair(hit.TrackID,TempMom.Phi()));
-	    }
-	}
-      for(auto hit : *AllHits[id_FMF2])
-	{
-	  auto it_find = validTrack.find(hit.TrackID);
-	  
-	  if(it_find != validTrack.end())
-	    {
-	      if(hit.LayerID == 0)
-		{
-		  TVector3 TempMom(hit.MomX,hit.MomY,hit.MomZ);
-		  auto it_phi = PhiPerTrack.find(hit.TrackID);
-		  if(it_phi != PhiPerTrack.end())
-		    {
-		      h_ParticlePhi->Fill((it_phi->second-TempMom.Phi())*TMath::RadToDeg(),hit.Pname.c_str(),1.);
-		    }
-		}
-	    }
-	}
-	  
-      // for(size_t id = 0;id<event->DaughterNames.size();++id)
-      // 	{
-      // 	  int trackID = event->DaughterTrackID[id];
-      // 	  std::cout<<"decayed : "<<event->DaughterNames[id]<<" #"<<trackID<<"\n";
+		     //TObjArray* obj_ret = new TObjArray(3);
+		     //obj_ret->Add(h_HitPatternX);
+		     //obj_ret->Add(h_HitPatternY);
+		     //obj_ret->Add(h_ParticlePhi);
+		     
+		     //return h_HitPatternX;//obj_ret;
+		     return std::make_tuple(h_HitPatternX,h_HitPatternY,h_ParticlePhi);
+		   };			    
+					    
 
-      // 	  for(auto& Hits :AllHits)
-      // 	    {
-      // 	      for(auto hit : *Hits)
-      // 		{
-      // 		  //hit.Print();
-      // 		  if(hit.TrackID==trackID)
-      // 		    {
-      // 		      std::cout<<"Branch: "<<Hits->GetBranchName()<<" "<<hit.HitPosX<<" "<<hit.HitPosY<<" "<<hit.HitPosZ<<" "<<hit.LayerID<<" "<<hit.Pname<<"\n";
-      // 		    }
-      // 		}
-      // 	    }
-      // 	}
+  // Create the pool of processes
+  //ROOT::TProcessExecutor workers(4);
+
+  // Process the TChain
+  //auto ObjArray = workers.ProcTree(*Chain, f_Process, "G4Tree");
+  //ObjArray->Print();
+  //tp.Process(f_Process);
+
+  TH2F* h_HitPatternXM = nullptr;
+  TH2F* h_HitPatternYM = nullptr;
+  TH2F* h_ParticlePhiM = nullptr;
+  std::tie(h_HitPatternXM,h_HitPatternYM,h_ParticlePhiM) = f_Process(reader);
+
+
+  //TH2F* h_HitPatternXM = static_cast<TH2F*>(ObjArray);
+  //TH2F* h_HitPatternYM = static_cast<TH2F*>(ObjArray->At(1));
+  //TH2F* h_ParticlePhiM = static_cast<TH2F*>(ObjArray->At(2));
   
-
-      
-    }
-
   auto plotHist = [](auto h, double min=-1, double max=-1, TString nameX="")
 		  {
 		    TString nameC(h->GetName());
@@ -228,20 +303,31 @@ void runData(const std::string& nameList, const std::set<std::string>& ParticleL
 		    c->Draw("colz");
 		  };
 
+
+
+
+  
+  //auto h_HitPatternXM = h_HitPatternX.Merge(); 
+  //auto h_HitPatternYM = h_HitPatternY.Merge(); 
+  //auto h_ParticlePhiM = h_ParticlePhi.Merge(); 
+
   if(nameOut == "")
     {
-      plotHist(h_HitPatternX);
-      plotHist(h_HitPatternY);
-      plotHist(h_ParticlePhi);
+      plotHist(h_HitPatternXM);
+      plotHist(h_HitPatternYM);
+      plotHist(h_ParticlePhiM);
     }
   else
     {
+      std::cout << "Saving: "<<nameOut<<"\n";
       TFile* f_out = new TFile(nameOut.c_str(),"RECREATE");
       f_out->cd();
-      h_HitPatternX->Write();
-      h_HitPatternY->Write();
-      h_ParticlePhi->Write();
+      h_HitPatternXM->Write();
+      h_HitPatternYM->Write();
+      h_ParticlePhiM->Write();
+      f_out->Close();
     }
+  std::cout<<"Done !\n";
 }
 
 
@@ -319,4 +405,16 @@ void runPhaseSpace(std::string nameF)
   c2->cd();
   h21->Draw("colz");
   
+}
+
+
+int main(int argc,char** argv)
+{
+  if (argc != 3)
+    {
+      std::cout<<"E> runData In.root Out.root\n";
+      return -1;
+    }
+
+  runData(argv[1],{},argv[2]);
 }
